cf53f0a1d4736ef0784116e5ac689e01cfc706ab
ashish <ashish.koirala004@gmail.com>
2026-07-13 11:54:41 +0545

    Grp113JulyCommit

--- src/main/java/io/herald/MySpringWeb/Controller/MappingClass.java
+++ src/main/java/io/herald/MySpringWeb/Controller/MappingClass.java
@@ -1,7 +1,9 @@
 package io.herald.MySpringWeb.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller  //Handles Http Requests : Get, Post, etc
 public class MappingClass {
@@ -23,6 +25,28 @@ public class MappingClass {
     public String loginPage()
     {
         return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String loginPost(HttpServletRequest request)
+    {
+String username= request.getParameter("username");
+String password = request.getParameter("password");
+        System.out.println(username);
+        System.out.println(password);
+
+        if(username.equals("admin") && password.equals("admin"))
+        {
+            return "home.html";
+        }
+
+            return "login.html";
+
+
+//When a form data does post request, HttpServletRequest obtains
+        //those data as parameters in controller.
+
+
     }
 
 }
